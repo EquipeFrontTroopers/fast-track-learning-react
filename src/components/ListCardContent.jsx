fae922c891454ce30517f38c6077bf588f25b628
aquistapace <59019615+aquistapace@users.noreply.github.com>
2020-11-24 09:06:19 -0300

    Finalizado Componente de Card e Componente Lista de Cards

---
+++ src/components/ListCardContent.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import CardContent from './CardContent';
+import './ListCardContent.css';
+
+class ListCardContent extends Component {
+  render() {
+    return (
+      <div className="central">
+        <ul className="list-card-container">
+          { this.props.listaConteudos.map((item, index) => (
+            <li key={index} className="list-card-item">
+              <CardContent
+                indice={index}
+                conteudo={item.conteudo}
+                tecnologia={item.tecnologia}
+                tipo={item.tipo}
+                url={item.url}
+                cargaHoraria={item.cargaHoraria}
+                prioridade={item.prioridade}
+                deletarCard={this.props.deletarCard}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ListCardContent;
